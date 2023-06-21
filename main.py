6695f02eb721e93dc19aef27c624d75f6ab3f4fd
Marius Jonika <s174411@titans.compute.dtu.dk>
2023-06-21 23:14:39 +0200

    pretrained dropout

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def get_args_parser():
                         help="Intermediate size of the feedforward layers in the transformer blocks")
     parser.add_argument('--hidden_dim', default=16, type=int,
                         help="Size of the embeddings (dimension of the transformer)")
-    parser.add_argument('--dropout', default=0, type=float,
+    parser.add_argument('--dropout', default=0.1, type=float,
                         help="Dropout applied in the transformer")
     parser.add_argument('--nheads', default=4, type=int,
                         help="Number of attention heads inside the transformer's attentions")
